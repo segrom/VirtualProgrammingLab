3612a2866cb0796c317b038524453bab1172e869
Segrom <segrom2001@mail.ru>
2024-04-06 15:22:29 +0300

    remove redundant page reloading in code editor

--- Application/wwwroot/js/codeAreaProvider.js
+++ Application/wwwroot/js/codeAreaProvider.js
@@ -1,27 +1,22 @@
 ﻿
-codeInput.registerTemplate("syntax-highlighted", codeInput.templates.prism(Prism, [
-    new codeInput.plugins.Indent(true, 4), // 2 spaces indentation
-    new codeInput.plugins.AutoCloseBrackets(),
-    new codeInput.plugins.Autocomplete()
-]));
+window.codeFunctions = {
+    addCodeAreaProvider: function () {
+        codeInput.registerTemplate("syntax-highlighted", codeInput.templates.prism(Prism, [
+            new codeInput.plugins.Indent(true, 4), // 2 spaces indentation
+            new codeInput.plugins.AutoCloseBrackets(),
+            new codeInput.plugins.Autocomplete()
+        ]));
 
-function codeAreaProvider() {
-    
+        let codeArea = document.querySelectorAll('code-input > textarea').item(0)
+        let codeProvider = document.querySelector("#code-provider");
 
-    let codeArea = document.querySelectorAll('code-input > textarea').item(0)
-    let codeProvider = document.querySelector("#code-provider");
-
-    codeArea.addEventListener('change', function () {
-        codeProvider.value = codeArea.value;
-        codeProvider.dispatchEvent(new Event('change'));
-    })
-    codeArea.addEventListener('keyup', function () {
-        codeProvider.value = codeArea.value;
-        codeProvider.dispatchEvent(new Event('change'));
-    })
+        codeArea.addEventListener('change', function () {
+            codeProvider.value = codeArea.value;
+            codeProvider.dispatchEvent(new Event('change'));
+        })
+        codeArea.addEventListener('keyup', function () {
+            codeProvider.value = codeArea.value;
+            codeProvider.dispatchEvent(new Event('change'));
+        })
+    }
 }
-
-setTimeout(
-    codeAreaProvider,
-    100
-)
