c402e36b771a7f6eaf09411c3ab1f867bd1cbdc0
Segrom <segrom2001@mail.ru>
2024-04-06 15:12:41 +0300

    fix code editor view

---
+++ Application/wwwroot/js/codeAreaProvider.js
@@ -0,0 +1,27 @@
+﻿
+codeInput.registerTemplate("syntax-highlighted", codeInput.templates.prism(Prism, [
+    new codeInput.plugins.Indent(true, 4), // 2 spaces indentation
+    new codeInput.plugins.AutoCloseBrackets(),
+    new codeInput.plugins.Autocomplete()
+]));
+
+function codeAreaProvider() {
+    
+
+    let codeArea = document.querySelectorAll('code-input > textarea').item(0)
+    let codeProvider = document.querySelector("#code-provider");
+
+    codeArea.addEventListener('change', function () {
+        codeProvider.value = codeArea.value;
+        codeProvider.dispatchEvent(new Event('change'));
+    })
+    codeArea.addEventListener('keyup', function () {
+        codeProvider.value = codeArea.value;
+        codeProvider.dispatchEvent(new Event('change'));
+    })
+}
+
+setTimeout(
+    codeAreaProvider,
+    100
+)
